aeedf7aa1b0ae556c5ea89318cfda80895fa3652
jdsan9 <jdsan9@users.noreply.github.com>
2015-08-31 11:47:34 -0400

    Update kbfix.meta.js

--- kbfix.meta.js
+++ kbfix.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Project page changes - url
+// @name         KB Updates
 // @namespace    https://crgstaff.com/
-// @version      0.5
+// @version      0.2
 // @description  See comments for change list
 // @author       You
-// @match        https://www.crgstaff.com/Projects/ProjectDetail_Tabbed.aspx*
+// @match        https://www.crgstaff.com/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @downloadURL  https://raw.githubusercontent.com/jdsan9/kbfix/master/kbfix.user.js
