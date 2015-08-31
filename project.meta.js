7c9c6ee4bd31d68681832e1535621ff9259c009a
jdsan9 <jdsan9@users.noreply.github.com>
2015-08-31 10:31:13 -0400

    Update project.meta.js

--- project.meta.js
+++ project.meta.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         Project page changes - url
+// @namespace    https://crgstaff.com/
+// @version      0.5
+// @description  See comments for change list
+// @author       You
+// @match        https://www.crgstaff.com/Projects/ProjectDetail_Tabbed.aspx*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @downloadURL  https://raw.githubusercontent.com/jdsan9/kbfix/master/project.meta.js
+// @updateURL    https://raw.githubusercontent.com/jdsan9/kbfix/master/project.meta.js
+// ==/UserScript==
+
 // Floating project toolbar
 var floatingToolbarCss = document.createElement("style");
 floatingToolbarCss.type = "text/css";
